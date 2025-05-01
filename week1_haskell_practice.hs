3d648b71bce46c9329faf5ad0775404d2fb24d16
Andres Lauder <80169543+ComicMaster41@users.noreply.github.com>
2025-05-01 14:17:12 -0700

    Create week1_haskell_practice.hs

---
+++ week1_haskell_practice.hs
@@ -0,0 +1,45 @@
+-- Haskell Practice Exercises Summary
+-- Cleaned and organized version of all progress, formatted for GitHub or local study.
+
+-- Week 1: Types and Simple Functions
+
+x :: Integer
+x = 10
+
+addThree :: Num a => a -> a
+addThree a = a + 3
+
+factorialBig :: Integer -> Integer
+factorialBig n = if (n == 0) then 1 else n * factorialBig (n - 1)
+
+safeDivide :: Double -> Double -> Maybe Double
+safeDivide _ 0 = Nothing
+safeDivide a d = Just (a / d)
+
+addWithBind = Just 5 >>= \x -> Just (x + 3)
+
+doubleIfEqual :: (Eq a, Num a) => a -> a -> a
+doubleIfEqual a b = if (a == b) then a * 2 else a
+
+squareList :: Num a => [a] -> [a]
+squareList xs = [x^2 | x <- xs]
+
+listPractice :: (Integral a) => [a] -> [a]
+listPractice xs = [x^2 | x <- [1..10], even x]
+
+wolf = [(x, y) | x <- [1, 2, 3], y <- [4, 5]]
+bear = [x | x <- [1..30], x `mod` 3 == 0, x `mod` 5 == 0]
+fruit = [length x | x <- ["cat", "apple", "banana"]]
+
+signLabel :: (Num a, Ord a) => a -> String
+signLabel x
+    | x < 0 = "Negative"
+    | x > 0 = "Positive"
+    | otherwise = "Zero"
+
+main :: IO ()
+main = do
+    print (squareList [1, 2, 3])
+    print (signLabel 12)
+
+list_comp = [x * 3 | x <- [1..20], odd x]
